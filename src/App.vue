9d72b470b48cf37226e8a1287ce0ff8a6ea68fa6
anil <anil.kumar1@kelltontech.com>
2018-09-19 12:27:57 +0530

    on the top

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+     <HeaderComponent msg="Header"  />
+    <HelloWorld msg="Listing"/>
+    
+  </div>
+</template>
+ 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+  
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="result">
+    <button v-on:click="fetchResult">Show</button>
+    <template>
+      <b-table striped hover :items="items"></b-table>
+    </template>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      result: "test",
+      items: [{
+        data: 'Loading...'
+      },
+
+      ]
+    }
+  },
+  methods: {
+    resultAdd: function() {
+      this.result = "new data is here"
+    },
+    fetchResult: function() {
+      axios.get('https://jsonplaceholder.typicode.com/users/')
+        .then((response) => {
+          // handle success
+          this.items = response.data
+        })
+        .catch(function(error) {
+          // handle error
+          this.items.data = "Some issue "
+        })
+    }
+  },
+  mounted() {
+    this.fetchResult()
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
