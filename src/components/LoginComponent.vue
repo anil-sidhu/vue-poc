446077c499731069ea64b96b33d49ff07c021315
anil <anil.kumar1@kelltontech.com>
2018-09-19 19:19:47 +0530

    add submit login api

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,39 +1,75 @@
 <template>
-<div class="hello">
-      <HeaderComponent />
+    <div class="hello">
+        <HeaderComponent />
+        <br>
+        <br>
+        <br>
+        <h1>Login Screen</h1>
+        <b-row class="justify-content-md-center">
+            <b-col sm="2">
+                <b-form-group>
+                    <b-form-input v-model="name" type="text" placeholder="Enter your name"></b-form-input>
+                </b-form-group>
+            </b-col>
+            <b-col sm="2">
+                <b-form-group>
+                    <b-form-input v-model="password" type="password" placeholder="Enter your name"></b-form-input>
+                </b-form-group>
+            </b-col>
+            <b-col sm="1">
+                <b-button v-on:click="submit">
+                    Login
+                </b-button>
 
-  <h1>Login Screen</h1>
-
-</div>
+            </b-col>
+        </b-row>
+        <p>{{name}}</p>
+    </div>
 </template>
 
 <script>
 const axios = require('axios');
 import HeaderComponent from './HeaderComponent.vue'
 export default {
-name: 'HelloWorld',
-props: {
-  msg: String
-},
- components: {
-    HeaderComponent,
-  },
-data: function() {
-  return {
-    result: "test",
-    items: [{
-      data: 'Loading...'
+    name: 'HelloWorld',
+    props: {
+        msg: String
     },
+    components: {
+        HeaderComponent,
+    },
+    data: function() {
+        return {
+            result: "test",
+            password: "",
+            name: "",
+            items: [{
+                data: 'Loading...'
+            },
 
-    ]
-  }
-},
-methods: {
-  resultAdd: function() {
-    this.result = "new data is here"
-  },
+            ]
+        }
+    },
+    methods: {
+        submit() {
+            console.warn("name", this.name, this.password)
+            axios.get('http://localhost:3004/user/7')
+                .then((response) => {
+                    // handle success
 
-},
+                    if (response.status == 200) {
+                        console.warn("response inside", response.status)
+                            this.$router.push({ name: 'profile', query: { redirect: '/profile' } });
+
+                    }
+                })
+                .catch(function(error) {
+                    // handle error
+                    this.items.data = "Some issue "
+                })
+        },
+
+    },
 
 }
 </script>
@@ -42,17 +78,16 @@ methods: {
 
 <style scoped>
 h3 {
-margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 
 ul {
-list-style-type: none;
-padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-display: inline-block;
-margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
-
 </style>
